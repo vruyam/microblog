baa040425bf54c214316ea3a393e57cfed05d107
Mayur <mayur.verlekar@parinati.in>
2019-11-24 17:32:31 +0530

    flask database conf

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import views
+from app import views, models
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': models.User, 'Post': models.Post}
