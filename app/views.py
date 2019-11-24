cb0740cf5eb6395cc598e91b252a2d7de6455b5a
Mayur <mayur.verlekar@parinati.in>
2019-11-24 15:22:05 +0530

    template inheritance

--- app/views.py
+++ app/views.py
@@ -2,6 +2,17 @@ from flask import render_template
 from app import app
 
 @app.route('/')
+@app.route('/index')
 def index():
 	user = {'username': 'Mayur'}
-	return render_template('index.html', title='Home', user=user)
+	posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+	return render_template('index.html', user=user, posts=posts)
