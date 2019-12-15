c7f137d2debc42bbdafb896aa590dd0254d53a20
mayur <vruyam@gmail.com>
2019-12-16 03:16:42 +0530

    url fix

--- app/blog/views.py
+++ app/blog/views.py
@@ -21,8 +21,8 @@ def explore():
 	page = request.args.get('page', 1, type=int)
 	posts = Post.query.order_by(Post.timestamp.desc()).paginate(
 		page, app.config['POSTS_PER_PAGE'], False)
-	next_url = url_for('blog.index', page=posts.next_num) if posts.has_next else None
-	prev_url = url_for('blog.index', page=posts.prev_num) if posts.has_prev else None
+	next_url = url_for('blog.explore', page=posts.next_num) if posts.has_next else None
+	prev_url = url_for('blog.explore', page=posts.prev_num) if posts.has_prev else None
 	return render_template('blog/index.html', title='Explore', posts=posts.items,next_url=next_url,prev_url=prev_url)
 
 @bp.route('/', methods=['GET', 'POST'])
--- app/email.py
+++ app/email.py
@@ -1,6 +1,6 @@
 from threading import Thread
 from flask_mail import Message
-from flask import render_template
+from flask import render_template,current_app
 from app import mail
 
 def send_async_email(app, msg):
@@ -11,7 +11,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email, args=(app, msg)).start()
+    Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
 
 
 def send_password_reset_email(user):
