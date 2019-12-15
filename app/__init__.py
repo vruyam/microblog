49a4c441a8d5deef83b77b00b45bc66fe3d6f6e6
mayur <vruyam@gmail.com>
2019-12-16 00:37:22 +0530

    Flask MAil and setting up python local mail server

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
@@ -10,10 +11,13 @@ import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
+mail = Mail(app)
 login = LoginManager(app)
+
+
 login.login_view = 'login'
 
 from app import views, models, errors
--- config.py
+++ config.py
@@ -6,10 +6,18 @@ class Config(object):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	MAIL_SERVER = os.environ.get('MAIL_SERVER')
-	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+	MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
+	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 8025)
 	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	ADMINS = ['mayur.verlekar@parinati.in',]
 	POSTS_PER_PAGE = 3
+
+# (venv) $ export MAIL_SERVER=smtp.googlemail.com
+# (venv) $ export MAIL_PORT=587
+# (venv) $ export MAIL_USE_TLS=1
+# (venv) $ export MAIL_USERNAME=<your-gmail-username>
+# (venv) $ export MAIL_PASSWORD=<your-gmail-password>
+
+# (venv) $ python -m smtpd -n -c DebuggingServer localhost:8025
